8a5d70a86b185d504118dd2656f5ed2b9f7305c3
Thomas Hsu <thomhsu@gmail.com>
2019-02-17 22:38:11 -0800

    Cleaned up server code

--- server/index.js
+++ server/index.js
@@ -1,67 +1,8 @@
 const express = require('express');
 const app = express();
-const cors = require('cors');
-const proxy = require('http-proxy-middleware');
 const port = process.env.PORT || 3000;
 
-
 app.use('/rooms/:listingId/', express.static(__dirname + '/../client/dist'));
-
-// app.use(
-//   '/rooms/1',
-//   proxy({
-//     target:'http://127.0.0.1:3001',
-//     changeOrigin: true
-//   })
-// );
-
-// app.use(
-//   '/rooms/checkout/1',
-//   proxy({
-//     target:'http://127.0.0.1:3002',
-//     changeOrigin: true
-//   })
-// );
-
-// app.use(
-//   '/rooms/bookings/1',
-//   proxy({
-//     target:'http://127.0.0.1:3002',
-//     changeOrigin: true
-//   })
-// );
-
-// app.use(
-//   '/rooms/1/images',
-//   proxy({
-//     target:'http://127.0.0.1:3001',
-//     changeOrigin: true
-//   })
-// );
-
-// app.use(
-//   '/rooms/reviews/recent',
-//   proxy({
-//     target:'http://127.0.0.1:3003',
-//     changeOrigin: true
-//   })
-// );
-
-// app.use(
-//   '/rooms/reviews/relevant',
-//   proxy({
-//     target:'http://127.0.0.1:3003',
-//     changeOrigin: true
-//   })
-// );
-
-// app.use(
-//   '/rooms/reviews/filter',
-//   proxy({
-//     target:'http://127.0.0.1:3003',
-//     changeOrigin: true
-//   })
-// );
 
 app.listen(port);
 console.log('Listening on port', port);

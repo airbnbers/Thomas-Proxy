e51df709d14f6312541b929fbabe2a6a08f71d63
Thomas Hsu <thomhsu@gmail.com>
2019-02-22 11:50:35 -0800

    Comment out checkout and review modules temporarily

--- server/index.js
+++ server/index.js
@@ -9,48 +9,48 @@ app.use('/rooms/:listingId/', express.static(__dirname + '/../public'));
 app.use(
   '/rooms/1/images',
   proxy({
-    target:'http://127.0.0.1:3001',
+    target:'http://bearbnb-photo-module.us-east-2.elasticbeanstalk.com/',
     changeOrigin: true
   })
 );
 
-app.use(
-  '/rooms/1',
-  proxy({
-    target:'http://127.0.0.1:3002',
-    changeOrigin: true
-  })
-);
+// app.use(
+//   '/rooms/1',
+//   proxy({
+//     target:'http://127.0.0.1:3002',
+//     changeOrigin: true
+//   })
+// );
 
-app.use(
-  '/rooms/bookings/1',
-  proxy({
-    target:'http://127.0.0.1:3002',
-    changeOrigin: true
-  })
-);
+// app.use(
+//   '/rooms/bookings/1',
+//   proxy({
+//     target:'http://127.0.0.1:3002',
+//     changeOrigin: true
+//   })
+// );
 
-app.use(
-  '/rooms/reviews/recent',
-  proxy({
-    target:'http://127.0.0.1:3003',
-    changeOrigin: true
-  })
-);
-app.use(
-  '/rooms/reviews/relevant',
-  proxy({
-    target:'http://127.0.0.1:3003',
-    changeOrigin: true
-  })
-);
-app.use(
-  '/rooms/reviews/filter',
-  proxy({
-    target:'http://127.0.0.1:3003',
-    changeOrigin: true
-  })
-);
+// app.use(
+//   '/rooms/reviews/recent',
+//   proxy({
+//     target:'http://127.0.0.1:3003',
+//     changeOrigin: true
+//   })
+// );
+// app.use(
+//   '/rooms/reviews/relevant',
+//   proxy({
+//     target:'http://127.0.0.1:3003',
+//     changeOrigin: true
+//   })
+// );
+// app.use(
+//   '/rooms/reviews/filter',
+//   proxy({
+//     target:'http://127.0.0.1:3003',
+//     changeOrigin: true
+//   })
+// );
 
 app.listen(port);
 console.log('Listening on port', port);
